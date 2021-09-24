bcfb35e26da297137a67acd4624ae3b5ca80cd1f
sudhansu <iamalexr9522@gmail.com>
2021-09-24 16:31:51 +0530

    24.0.2

--- src/components/Home/Carousel.js
+++ src/components/Home/Carousel.js
@@ -1,103 +1,100 @@
 import React from "react";
-import { Fade } from "react-reveal";
 const Carousel = () => {
   return (
     <div>
-      <Fade right>
-        <div
-          id="carouselExampleCaptions"
-          className="carousel slide"
-          data-bs-ride="carousel"
-        >
-          <div className="carousel-indicators">
-            <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="0"
-              className="active"
-              aria-current="true"
-              aria-label="Slide 1"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="1"
-              aria-label="Slide 2"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="2"
-              aria-label="Slide 3"
-            ></button>
-          </div>
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img
-                src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600516/VIDEO%20GALLERY/HOME/home1_nwitpv.jpg"
-                alt=""
-                className="d-block w-100"
-              />
-              <div className="carousel-caption d-none d-md-block">
-                {/* <h5>First slide label</h5>
+      <div
+        id="carouselExampleCaptions"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600516/VIDEO%20GALLERY/HOME/home1_nwitpv.jpg"
+              alt=""
+              className="d-block w-100"
+            />
+            <div className="carousel-caption d-none d-md-block">
+              {/* <h5>First slide label</h5>
                 <p>
                   Some representative placeholder content for the first slide.
                 </p> */}
-              </div>
             </div>
-            <div className="carousel-item">
-              <img
-                src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600516/VIDEO%20GALLERY/HOME/home5_y6lufc.jpg"
-                alt=""
-                className="d-block w-100"
-              />
-              <div className="carousel-caption d-none d-md-block">
-                {/* <h5>Second slide label</h5>
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600516/VIDEO%20GALLERY/HOME/home5_y6lufc.jpg"
+              alt=""
+              className="d-block w-100"
+            />
+            <div className="carousel-caption d-none d-md-block">
+              {/* <h5>Second slide label</h5>
                 <p>
                   Some representative placeholder content for the second slide.
                 </p> */}
-              </div>
             </div>
-            <div className="carousel-item">
-              <img
-                src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600516/VIDEO%20GALLERY/HOME/home8_fjpskc.jpg"
-                alt=""
-                className="d-block w-100"
-              />
-              <div className="carousel-caption d-none d-md-block">
-                {/* <h5>Third slide label</h5>
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600516/VIDEO%20GALLERY/HOME/home8_fjpskc.jpg"
+              alt=""
+              className="d-block w-100"
+            />
+            <div className="carousel-caption d-none d-md-block">
+              {/* <h5>Third slide label</h5>
                 <p>
                   Some representative placeholder content for the third slide.
                 </p> */}
-              </div>
             </div>
           </div>
-          <button
-            className="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon me-auto bg-dark opacity-50  rounded-pill"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button
-            className="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon ms-auto  bg-dark opacity-50 rounded-pill"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Next</span>
-          </button>
         </div>
-      </Fade>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon me-auto bg-dark opacity-50  rounded-pill"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon ms-auto  bg-dark opacity-50 rounded-pill"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Fade } from "react-reveal";
 import Carousel from "./Carousel";
 import GetStarted from "./GetStarted";
 import OurFeatures from "./OurFeatures";
@@ -11,61 +10,59 @@ const Home = ({ setLModal }) => {
         <div className="row row-cols-1 d-flex justify-content-between">
           {/* 9 Images Grid 3x3 */}
           <div className="col-md-3 col-sm-12 my-auto ">
-            <Fade left>
-              <div className="row row-cols-3 shadow-lg">
-                <img
-                  src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600518/VIDEO%20GALLERY/HOME/home6_ymk88x.jpg"
-                  alt=""
-                  className="col m-0 p-0"
-                />
-                <img
-                  src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600518/VIDEO%20GALLERY/HOME/home7_figboe.jpg"
-                  alt=""
-                  className="col m-0 p-0"
-                />
-                <img
-                  src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600517/VIDEO%20GALLERY/HOME/home9_mbsmzu.jpg"
-                  alt=""
-                  className="col m-0 p-0"
-                />
-                <img
-                  src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600517/VIDEO%20GALLERY/HOME/home1_nwitpv.jpg"
-                  alt=""
-                  className="col m-0 p-0"
-                />
-                <img
-                  src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600517/VIDEO%20GALLERY/HOME/home4_qg3lxi.jpg"
-                  alt=""
-                  className="col m-0 p-0"
-                />
-                <img
-                  src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600517/VIDEO%20GALLERY/HOME/home2_siqxhi.jpg"
-                  alt=""
-                  className="col m-0 p-0"
-                />
-                <img
-                  src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600517/VIDEO%20GALLERY/HOME/home5_y6lufc.jpg"
-                  alt=""
-                  className="col m-0 p-0"
-                />
-                <img
-                  src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600516/VIDEO%20GALLERY/HOME/home8_fjpskc.jpg"
-                  alt=""
-                  className="col m-0 p-0"
-                />
-                <img
-                  src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600516/VIDEO%20GALLERY/HOME/home3_aogp3k.jpg"
-                  alt=""
-                  className="col m-0 p-0"
-                />
-              </div>
-              <div className="text-center my-3 text-light mx-auto">
-                <h3 className="text-warning">Welcome to Prilix</h3>
-                <p>Join Prilix to watch the latest movies, TV shows</p>
-                {/* Get Started Button */}
-                <GetStarted setLModal={setLModal} />
-              </div>
-            </Fade>
+            <div className="row row-cols-3 shadow-lg">
+              <img
+                src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600518/VIDEO%20GALLERY/HOME/home6_ymk88x.jpg"
+                alt=""
+                className="col m-0 p-0"
+              />
+              <img
+                src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600518/VIDEO%20GALLERY/HOME/home7_figboe.jpg"
+                alt=""
+                className="col m-0 p-0"
+              />
+              <img
+                src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600517/VIDEO%20GALLERY/HOME/home9_mbsmzu.jpg"
+                alt=""
+                className="col m-0 p-0"
+              />
+              <img
+                src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600517/VIDEO%20GALLERY/HOME/home1_nwitpv.jpg"
+                alt=""
+                className="col m-0 p-0"
+              />
+              <img
+                src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600517/VIDEO%20GALLERY/HOME/home4_qg3lxi.jpg"
+                alt=""
+                className="col m-0 p-0"
+              />
+              <img
+                src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600517/VIDEO%20GALLERY/HOME/home2_siqxhi.jpg"
+                alt=""
+                className="col m-0 p-0"
+              />
+              <img
+                src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600517/VIDEO%20GALLERY/HOME/home5_y6lufc.jpg"
+                alt=""
+                className="col m-0 p-0"
+              />
+              <img
+                src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600516/VIDEO%20GALLERY/HOME/home8_fjpskc.jpg"
+                alt=""
+                className="col m-0 p-0"
+              />
+              <img
+                src="https://res.cloudinary.com/djczbozsv/image/upload/v1630600516/VIDEO%20GALLERY/HOME/home3_aogp3k.jpg"
+                alt=""
+                className="col m-0 p-0"
+              />
+            </div>
+            <div className="text-center my-3 text-light mx-auto">
+              <h3 className="text-warning">Welcome to Prilix</h3>
+              <p>Join Prilix to watch the latest movies, TV shows</p>
+              {/* Get Started Button */}
+              <GetStarted setLModal={setLModal} />
+            </div>
           </div>
           {/* Carousel */}
           <div className="col-md-9 m-0 p-0">
